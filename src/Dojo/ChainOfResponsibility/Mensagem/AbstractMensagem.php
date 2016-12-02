1b745a2676ecbbac95eab1cfb70d54c90c4203fa
Rubens Takiguti Ribeiro <rubens.ribeiro@manageronline.com.br>
2016-12-02 08:02:35 -0200

    Refatoracao para nao usar fila

--- src/Dojo/ChainOfResponsibility/Mensagem/AbstractMensagem.php
+++ src/Dojo/ChainOfResponsibility/Mensagem/AbstractMensagem.php
@@ -7,22 +7,10 @@ use Dojo\ChainOfResponsibility\DadosUsuario;
 abstract class AbstractMensagem
 {
     protected $usuario;
-    protected $valorPadrao;
-    protected $proximo;
 
     abstract public function podeExibir();
 
     abstract public function getDados();
-
-    public function setValorPadrao($valorPadrao)
-    {
-        $this->valorPadrao = $valorPadrao;
-    }
-
-    public function setProximo(self $proximo)
-    {
-        $this->proximo = $proximo;
-    }
 
     public function setUsuario(DadosUsuario $usuario)
     {
@@ -32,17 +20,5 @@ abstract class AbstractMensagem
     public function getUsuario()
     {
         return $this->usuario;
-    }
-
-    public function getDadosMensagem(DadosUsuario $usuario)
-    {
-        $this->setUsuario($usuario);
-        if ($this->podeExibir()) {
-            return $this->getDados();
-        }
-        if ($this->proximo instanceof self) {
-            return $this->proximo->getDadosMensagem($usuario);
-        }
-        return $this->valorPadrao;
     }
 }
--- src/Dojo/ChainOfResponsibility/VerificadorMensagemPrioritaria.php
+++ src/Dojo/ChainOfResponsibility/VerificadorMensagemPrioritaria.php
@@ -2,30 +2,41 @@
 
 namespace Dojo\ChainOfResponsibility;
 
+use Dojo\ChainOfResponsibility\Mensagem\AbstractMensagem;
+
 final class VerificadorMensagemPrioritaria
 {
-    protected $fila;
+    protected $mensagens;
+    protected $valorPadrao;
 
     public function __construct(array $mensagens, $valorPadrao = null)
+    {
+        $this->validarMensagens($mensagens);
+        $this->mensagens = $mensagens;
+        $this->valorPadrao = $valorPadrao;
+    }
+
+    public function getDadosMensagem(DadosUsuario $usuario)
+    {
+        foreach ($this->mensagens as $mensagem) {
+            $mensagem->setUsuario($usuario);
+            if ($mensagem->podeExibir()) {
+                return $mensagem->getDados();
+            }
+        }
+        return $this->valorPadrao;
+    }
+
+    private function validarMensagens(array $mensagens)
     {
         if (empty($mensagens)) {
             throw new \InvalidArgumentException('Array de mensagens nao pode ser vazio');
         }
 
-        $this->fila = array_shift($mensagens);
-
-        $referencia = $this->fila;
-        while (!empty($mensagens)) {
-            $mensagem = array_shift($mensagens);
-            $referencia->setProximo($mensagem);
-            $referencia = $mensagem;
+        foreach ($mensagens as $mensagem) {
+            if (!($mensagem instanceof AbstractMensagem)) {
+                throw new \InvalidArgumentException('Array contendo valor invalido');
+            }
         }
-
-        $referencia->setValorPadrao($valorPadrao);
-    }
-
-    public function getDadosMensagem(DadosUsuario $usuario)
-    {
-        return $this->fila->getDadosMensagem($usuario);
     }
 }
